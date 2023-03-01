ff0fddd693b5e31091138250a48bf46863be8a9f
rambutan <pride.kraievskyi@gmail.com>
2023-03-01 13:00:13 +0200

    added fix

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,6 +5,7 @@ export const Layout: React.FC<LayoutProps> = ({ children, ...props }) => {
 	return (
 		<div className="container mx-auto py-5" {...props}>
 			<div className="text-4xl font-black">Materials</div>
+      
 		</div>
 	);
 };
